6d418c7575399852f49be49b7869dfe15b0421e4
felixdae <felixdae@gmail.com>
2023-12-09 07:17:39 +0000

    add start script

---
+++ start_train.sh
@@ -0,0 +1,12 @@
+set -xeuo pipefail
+
+if [ "$1" = "ddp" ];then
+    # ddp
+    exec torchrun --nnodes=1 --nproc_per_node=2 --master_addr=localhost --master_port=30601 --node_rank=0 -m char_llm.ddp_train --data_file=./char_llm/shakespeare.txt --batch_size=2 --wandb_project=gpt-train-lab --max_epoch=500
+elif [ "$1" = "fsdp" ];then
+    # fsdp
+    exec torchrun --nnodes=1 --nproc_per_node=2 --master_addr=localhost --master_port=30601 --node_rank=0 -m char_llm.fsdp_train --data_file=./char_llm/shakespeare.txt --batch_size=2 --wandb_project=gpt-train-lab --max_epoch=500
+else
+    # vanilla
+    exec python -m char_llm.train --data_file=./char_llm/shakespeare.txt --batch_size=64 --model_type=gpt --wandb_project=gpt-train-lab
+fi;
