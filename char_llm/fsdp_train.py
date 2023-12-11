db28e0e73d17810d3af544065084f11e86af47f7
felixdae <felixdae@gmail.com>
2023-12-11 12:19:41 +0000

    lint fsdp, and save oom

--- char_llm/fsdp_train.py
+++ char_llm/fsdp_train.py
@@ -1,37 +1,40 @@
 import argparse
-from dataclasses import asdict
-import os
-from typing import Any
-from loguru import logger
 import functools
-import torch
-from torch.utils.data import Dataset, DataLoader
-from tqdm import tqdm
-from char_llm.distributed import build_model, cleanup, init_distributed, prepare_dataloader
-from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
-from torch.distributed.fsdp.fully_sharded_data_parallel import CPUOffload
-from torch.distributed.fsdp.wrap import size_based_auto_wrap_policy
+import os
 
-from char_llm.model_gpt import GPT, GPTConfig
+import torch
+from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
+from torch.distributed.fsdp.wrap import size_based_auto_wrap_policy
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from char_llm.distributed import (
+    build_model,
+    cleanup,
+    init_distributed,
+    prepare_dataloader,
+)
+from char_llm.model_gpt import GPT
 from char_llm.util import TrainArgs
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--eval_interval', type=int, default=50)
-    parser.add_argument('--batch_size', type=int, required=True)
-    parser.add_argument('--max_epoch', type=int, default=500)
-    parser.add_argument('--wandb_project', type=str, required=True)
-    parser.add_argument('--lr', type=float, default=3e-4, help='learning rate')
-    parser.add_argument('--data_file', type=str, help='data filename')
+    parser.add_argument("--eval_interval", type=int, default=50)
+    parser.add_argument("--batch_size", type=int, required=True)
+    parser.add_argument("--max_epoch", type=int, default=500)
+    parser.add_argument("--wandb_project", type=str, required=True)
+    parser.add_argument("--lr", type=float, default=3e-4, help="learning rate")
+    parser.add_argument("--data_file", type=str, help="data filename")
     args = parser.parse_args()
     return args
 
 
 def train(max_epoch: int, train_dl: DataLoader, model: GPT, lr: float, device):
-    local_rank = int(os.environ['LOCAL_RANK'])
-    my_auto_wrap_policy = functools.partial(size_based_auto_wrap_policy,
-                                            min_num_params=20000)
+    # local_rank = int(os.environ["LOCAL_RANK"])
+    my_auto_wrap_policy = functools.partial(
+        size_based_auto_wrap_policy, min_num_params=20000
+    )
     model = FSDP(
         model,
         auto_wrap_policy=my_auto_wrap_policy,
@@ -55,7 +58,7 @@ def train(max_epoch: int, train_dl: DataLoader, model: GPT, lr: float, device):
 def main():
     args = get_args()
     data_file = args.data_file
-    device = 'cuda'
+    device = "cuda"
     train_args = TrainArgs(
         batch_size=args.batch_size,
         eval_interval=args.eval_interval,
@@ -68,12 +71,13 @@ def main():
     init_distributed()
 
     vocab_size, decode, train_dl, test_dl = prepare_dataloader(
-        data_file, train_args.block_size, train_args.batch_size)
+        data_file, train_args.block_size, train_args.batch_size
+    )
     model = build_model(train_args, vocab_size, device)
     train(args.max_epoch, train_dl, model, train_args.learning_rate, device)
 
     cleanup()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
