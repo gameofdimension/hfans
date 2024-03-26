2fbb1d859f57bffde93c1137453513a85f1de957
gameofdimension <felixdae@gmail.com>
2024-03-26 08:37:48 +0000

    build 3 modules model

---
+++ sd_train/xl3m.py
@@ -0,0 +1,104 @@
+import sys
+
+import torch
+from diffusers import DDPMScheduler, UNet2DConditionModel  # type: ignore
+from tqdm import tqdm
+from transformers import CLIPTextModel, CLIPTextModelWithProjection
+
+
+class SDXLModel(torch.nn.Module):
+    def __init__(
+            self, unet: torch.nn.Module,
+            text_encoder_one: torch.nn.Module,
+            text_encoder_two: torch.nn.Module):
+        super(SDXLModel, self).__init__()
+        self.unet = unet
+        self.text_encoder_one = text_encoder_one
+        self.text_encoder_two = text_encoder_two
+
+    def forward(
+            self, timesteps: torch.Tensor, input_ids1: torch.Tensor,
+            input_ids2: torch.Tensor, noisy_latents: torch.Tensor):
+        pass
+
+
+def make_model(checkpoint: str, device):
+    unet: torch.nn.Module = UNet2DConditionModel.from_pretrained(  # type: ignore # noqa
+        checkpoint, subfolder='unet').to(device)  # type: ignore
+    unet.requires_grad_(True)
+    unet.train()
+    text_encoder_one: torch.nn.Module = CLIPTextModel.from_pretrained(  # type: ignore # noqa
+        checkpoint, subfolder="text_encoder"
+    )
+    text_encoder_one.requires_grad_(True)
+    text_encoder_one.train()
+    text_encoder_two: torch.nn.Module = CLIPTextModelWithProjection.from_pretrained(  # type: ignore # noqa
+        checkpoint, subfolder="text_encoder_2"
+    )
+    text_encoder_two.requires_grad_(True)
+    text_encoder_two.train()
+    noise_scheduler = DDPMScheduler.from_pretrained(
+        checkpoint, subfolder="scheduler")
+    sdxl_model = SDXLModel(unet, text_encoder_one, text_encoder_two)
+    return sdxl_model, noise_scheduler
+
+
+def batch_data(noise_scheduler, batch_size, device):
+    model_input = torch.randn(
+        (batch_size, 4, 64, 64), device=device)
+    timesteps = torch.randint(
+        0, noise_scheduler.config.num_train_timesteps,
+        (batch_size,), device=device
+    )
+    noise = torch.randn_like(model_input)
+    noisy_model_input = noise_scheduler.add_noise(
+        model_input, noise, timesteps)
+    # prompt_embeds = torch.randn(
+    #     (batch_size, 77, 2048), device=device)
+    unet_added_conditions = {
+        "time_ids": torch.randn((batch_size, 6), device=device),
+        # "text_embeds": torch.randn((batch_size, 1280), device=device),
+    }
+    return (
+        noisy_model_input, timesteps, prompt_embeds,
+        unet_added_conditions, noise
+    )
+
+
+def train(model: SDXLModel, optimizer, noise_scheduler, batch_size, device):
+    total_step = 1000000
+    for step in tqdm(range(total_step)):
+
+        batch = batch_data(noise_scheduler, batch_size, device)
+        noisy_model_input, timesteps, prompt_embeds, \
+            unet_added_conditions, noise = batch
+
+        # model_pred = unet(
+        #     noisy_model_input,
+        #     timesteps,
+        #     prompt_embeds,
+        #     added_cond_kwargs=unet_added_conditions,
+        #     return_dict=False,
+        # )[0]
+
+        assert noise_scheduler.config.prediction_type == "epsilon"
+        loss = torch.nn.functional.mse_loss(
+            model_pred.float(), noise.float(), reduction="mean")
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+
+def main():
+    lr = 2e-5
+    batch_size = int(sys.argv[1])
+    device = sys.argv[2]
+    checkpoint = 'stabilityai/stable-diffusion-xl-base-1.0'
+    # checkpoint = '/root/model-repo/llm-stable-diffusion-xl-base-1.0'
+    model, noise_scheduler = make_model(checkpoint, device)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+    train(model, optimizer, noise_scheduler, batch_size, device)
+
+
+if __name__ == "__main__":
+    main()
